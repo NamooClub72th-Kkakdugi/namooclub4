4a5be7408996a11aedb1edabd6ba7743b05d94ce
kosta-18 <kosta-18@kosta-18-PC>
2014-04-21 15:44:31 +0900

    dependency

---
+++ namoo-club4-web/src/main/java/com/namoo/club/web/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.namoo.club.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.namoo.club.service.facade.UserService;
+
+@Controller
+public class UserController {
+	//
+	private UserService service;
+}
