f1bfb6dc728fffb60d61ddceb559a76341501457
kosta-18 <kosta-18@kosta-18-PC>
2014-04-21 15:47:45 +0900

    a

--- namoo-club4-web/src/main/java/com/namoo/club/web/controller/UserController.java
+++ namoo-club4-web/src/main/java/com/namoo/club/web/controller/UserController.java
@@ -1,11 +1,21 @@
 package com.namoo.club.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.namoo.club.service.facade.UserService;
 
 @Controller
 public class UserController {
 	//
+	@Autowired
 	private UserService service;
+	
+	@RequestMapping(value="/main", method=RequestMethod.GET)
+	public String login() {
+		//
+		return "/user/login";
+	}
 }
